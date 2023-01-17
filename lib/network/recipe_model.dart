9b0c7aa8b020c24b97ed5233f47ba1871854d018
jiho <ghrms80@gmail.com>
2023-01-18 00:43:54 +0900

    Creating model classes

---
+++ lib/network/recipe_model.dart
@@ -0,0 +1,10 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class APIRecipeQuery {
+  // TODO: Add APIRecipeQuery. fromJson
+}
+
+// TODO: Add @JsonSerializable() class APIHits
+// TODO: Add @JsonSerializable() class APIRecipe
+// TODO: Add @JsonSerializable() class APIIngredients
